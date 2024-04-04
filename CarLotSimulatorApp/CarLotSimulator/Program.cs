83bc03673f1b1062f633287b0399509d261e9935
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-04-03 23:21:29 -0400

    more cars

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CarLotSimulator
 {
@@ -17,6 +23,32 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+
+            var andrewsCar = new Car();
+            andrewsCar.Year = 2013;
+            andrewsCar.Make = "Ford";
+            andrewsCar.Model = "GT";
+            andrewsCar.EngineNoise = "brrpp";
+            andrewsCar.HonkNoise = "meepmeep";
+            andrewsCar.IsDrivable = true;
+
+            var batMansCar = new Car()
+            {
+                Year = 2019,
+                Make = "Tesla",
+                Model = "Plaid",
+                EngineNoise = "zzz",
+                HonkNoise = "Thud",
+                IsDrivable = false,
+            };
+
+            var superMansCar = new Car(2019, "Ferrari", "SuperFast", "GRRRR", "BowBow", true);
+
+            andrewsCar.MakeEngineNoise();
+            batMansCar.MakeEngineNoise();
+            superMansCar.MakeEngineNoise();
+            
+
 
             //*************BONUS*************//
 
